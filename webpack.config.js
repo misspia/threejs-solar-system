66eeefb7528d52de21639b68d5784dd3bb426bcf
misspia <pia_leung@hotmail.com>
2017-08-16 23:57:21 -0400

    js linter module

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,14 @@ var path = require('path');
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+	filename: 'bundle.js',
+	path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+	rules: [{
+	  test: /\.js$/, // Run the loader on all .js files
+	  exclude: /node_modules/, // ignore all files in the node_modules folder
+	  use: 'jshint-loader'
+	}]
   }
 };
