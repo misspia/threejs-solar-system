7cd5bc11e777e104c13bb3f8dfc967be37d9880c
misspia <pia_leung@hotmail.com>
2017-08-19 13:37:57 -0400

    sass loader

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import test from './js/test.js';
+import testShader from './shaders/test.glsl';
+import './scss/main.scss';
+
+console.log('sup world!????????', test());
+console.log('test shader', testShader);
--- src/js/index.js
+++
@@ -1,4 +0,0 @@
-import test from './test.js';
-import testShader from '../shaders/test.glsl';
-console.log('sup world!????????', test());
-console.log('test shader', testShader);
