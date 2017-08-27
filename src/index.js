2fe58911c9b7c0730e57676ebf9b599b6b39f0a1
misspia <pia_leung@hotmail.com>
2017-08-26 23:02:26 -0400

    modularize init/create methods, create utils file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './scss/main.scss';
-
 import app from './js/app.js';
-// import testShader from './shaders/test.glsl';
 
 // http://maplestory.nexon.net/game/classes-and-jobs/2892/luminous
 
