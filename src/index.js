5cdf751776338b603a339c0480d19ffe2ee164b7
misspia <pia_leung@hotmail.com>
2017-11-11 20:11:00 -0800

    init + set up visualize function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './scss/main.scss';
-import App from './js/app.js';
+// import App from './js/app.js';
+import Visualize from './js/visualize.js';
+
 import './scss/main.scss';
 
-
+Visualize();
 
 // http://maplestory.nexon.net/game/classes-and-jobs/2892/luminous
 
-App();
+//App();
