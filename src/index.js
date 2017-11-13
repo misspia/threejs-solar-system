edd6ef34b8581a39462d1900cb4331c2c5b8465f
misspia <pia_leung@hotmail.com>
2017-11-12 20:16:02 -0800

    move THREE set up to index, create class for cube + rotation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
+// http://maplestory.nexon.net/game/classes-and-jobs/2892/luminous
+import * as THREE from 'three';
+
 import './scss/main.scss';
-// import App from './js/app.js';
 import Visualize from './js/visualize.js';
 
 import './scss/main.scss';
 
-Visualize();
+const entryElement = document.body;
 
-// http://maplestory.nexon.net/game/classes-and-jobs/2892/luminous
+const viewport = { height: window.innerHeight, width: window.innerWidth };
+const aspectRatio = viewport.width / viewport.height;
 
-//App();
+window.addEventListener( 'resize', () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  renderer.setSize( width, height );
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix;
+})
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, aspectRatio, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( viewport.width, viewport.height );
+entryElement.appendChild( renderer.domElement );
+
+camera.position.z = 3;
+
+const render = () => {
+  renderer.render( scene, camera );
+}
+
+const cube = new Visualize(scene);
+const animate = () => {
+  requestAnimationFrame( animate );
+  cube.update();
+  render();
+}
+
+animate();
--- src/js/visualize.js
+++ src/js/visualize.js
@@ -1,44 +1,14 @@
 import * as THREE from 'three';
 
-const entryElement = document.body;
-
-const viewport = { height: window.innerHeight, width: window.innerWidth };
-const aspectRatio = viewport.width / viewport.height;
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, aspectRatio, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( viewport.width, viewport.height );
-entryElement.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
-const cube = new THREE.Mesh( geometry, material );
-scene.add(cube);
-camera.position.z = 3;
-
-const update = () => {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.005;
+function Visualize(scene) {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add(cube);
+  this.update = () => {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.005;
+  }
 }
 
-window.addEventListener( 'resize', () => {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-  renderer.setSize( width, height );
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix;
-})
-const render = () => {
-  renderer.render( scene, camera );
-}
-
-const animate = () => {
-  requestAnimationFrame( animate );
-  update();
-  render();
-}
-
-
-export default animate;
+export default Visualize;
