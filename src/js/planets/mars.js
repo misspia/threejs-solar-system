586b344cb7960b72bd6e5e9a53b836d02226e50f
misspia <pia_leung@hotmail.com>
2017-12-28 17:38:30 -0800

    initialize mars object

---
+++ src/js/planets/mars.js
@@ -0,0 +1,14 @@
+import Planet from './planet.js';
+import {mars as metadata} from '../solarSystem.metadata.js';
+
+const Mars = new Planet;
+
+const base = {
+  color: metadata.color,
+  radius: metadata.radius,
+  detail: 1
+};
+
+Mars.addBase(base);
+
+export default Mars;
