ab165e49edc9f36f6bba0578282ad9ede6b61b27
misspia <pia_leung@hotmail.com>
2017-12-29 13:54:13 -0800

    create controls directory, move all parts of core SS scene into SS folder

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
+import $ from 'jquery';
 import * as THREE from 'three'
 import SceneManager from './sceneManager.js';
-import SolarSystem from './solarSystem.js';
+import SolarSystem from './solarSystem/solarSystem.js';
 
-const entryElement = document.getElementById('app');
+const entryElement = $('#app');
 entryElement.height = window.innerHeight;
 entryElement.width = window.innerWidth;
 
@@ -14,12 +15,12 @@ app.addOrbitControls();
 app.addPointLight({x: 200, y: 200, z: 400});
 app.addAmbientLight();
 
-entryElement.appendChild(app.renderer.domElement);
+entryElement.append(app.renderer.domElement);
 
 const timeFactor = 1;
 const SS = new SolarSystem(app);
 SS.addAllBodies();
-SS.timeFactor = 1;
+SS.timeFactor = 40;
 
 const GameLoop = ()  => {
   requestAnimationFrame(GameLoop);
